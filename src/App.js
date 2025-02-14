64e281909b60f2b39c3c6d818ea102c5b971d26e
Ronak-dx <ronakkaushik.dx@gmail.com>
2025-02-14 18:13:54 +0530

    Tiwil-pwa-concept

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
-import React, { useState, useEffect } from "react";
-
-const App = () => {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import SignIn from "./components/Signin";
+import SignUp from "./components/SignUp";
+import Events from "./components/Eventlist";  // Protected Page
+// import Invitations from "./components/Invitations"; // Protected Page
+import PrivateRoute from "./components/Authentication/Privateroute";
+import UserProfile from "./components/UserProfile";
+import Dashboard from "./components/Dashboard/Dashboard"
+import Home from "./components/Home";
+import AddInformationForm from "./components/AdditionalInfo";
+import Starting from "./components/OnbaordingScreen/Starting";
+import LabTabs from "./components/Tabbingplans/Planandclebs";
+import EventDetails from "./components/eventDetails";
+import WishlistForm from "./components/Wishlist/addWishlist";
+import HomePage from "./components/Home";
+import { useState,useEffect } from "react";
+import UserEventSection from "./components/userview/userEventsection";
+import NotificationList from "./components/Notifications/Notification";
+import WishlistDetails from "./components/userview/wishlistDetails";
+import PoolingWish from "./components/Pooloptions/Createpool";
+import MyProfile from "./components/Profile/Myprofile";
+import FamilyList from "./components/Profile/Familyinfo";
+import LoginPage from "./components/Login";
+import InvitationList from "./components/InvitationList";
+import InvitePag from "../src/components/InvitPage";
+function App() {
   const [deferredPrompt, setDeferredPrompt] = useState(null);
   const [showInstallButton, setShowInstallButton] = useState(false);
 
@@ -33,7 +58,9 @@ const App = () => {
     }
   };
 
+
   return (
+    <>
     <div style={{ textAlign: "center", padding: "20px" }}>
       <h1>PWA Concept App</h1>
       <p>Install this app on your home screen for a better experience.</p>
@@ -43,7 +70,42 @@ const App = () => {
         </button>
       )}
     </div>
-  );
-};
+    <Router>
+      <Routes>
+<Route path="/" element={<SignIn/>}></Route>
+        {/* Public Routes */}
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<HomePage/>}/>
+        {/* Private Routes (Protected) */}
+        <Route element={<PrivateRoute />}>
+          
+        <Route path="/userprofile" element={<UserProfile/>}></Route>
+          {/* <Route path="/invitations" element={<Invitations />} /> */}
+        </Route>
+    
+        <Route path="/events" element={<Events />} />
+        {/* Default Route */}
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/additionalinfo" element={<AddInformationForm/>} />
+        <Route path="/plans" element={<LabTabs/>} />
+        <Route path="/plandetails/:eventId" element={<EventDetails />} />
+        <Route path='/addtowish' element={<WishlistForm/>}/>
+        <Route path="/invitationdetails/:eventId" element={<UserEventSection/>}/>
+        <Route path="/notifications" element={<NotificationList/>}/>
+        <Route path='/wishlistdetails/:itemId'element={<WishlistDetails/>}></Route>
+        <Route path='/createpool' element={<PoolingWish/>}></Route>
+        <Route path='/userdetail' element={<MyProfile/>}/>
+        <Route path="/familyinfo"  element={<FamilyList/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/invitation' element={<InvitationList/>}/>
+        <Route path="/invite" element={<InvitePag/>} />
 
-export default App
+      </Routes>
+    </Router>
+</>
+  );
+}
+
+export default App;
