4676590bac0148d871bd0b9e5a06498216892bcc
Ronak-dx <ronakkaushik.dx@gmail.com>
2025-03-30 13:27:09 +0530

    server

--- src/chatsection/ChatRoom.js
+++ src/chatsection/ChatRoom.js
@@ -81,7 +81,7 @@ const ChatRoom = () => {
   }, [page]);
 
   useEffect(() => {
-    socketRef.current = io("wss://45.77.110.58:6543", {
+    socketRef.current = io("wss://45.77.110.58:6543/socket.io/", {
       query: { token },
       transports: ["websocket","polling"],
       withCredentials: true
