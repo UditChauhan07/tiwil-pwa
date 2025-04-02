2c218a0c122f9cfdd7e79f53a0f22930725393d6
Ronak-dx <ronakkaushik.dx@gmail.com>
2025-04-02 10:16:53 +0530

    Update ChatRoom.js

--- src/chatsection/ChatRoom.js
+++ src/chatsection/ChatRoom.js
@@ -6,7 +6,7 @@ import io from "socket.io-client";
 import styles from "./ChatRoom.module.css";
 import { FiArrowLeft } from "react-icons/fi";
 
-const SOCKET_SERVER_URL = "https://45.77.110.58:6543";
+const SOCKET_SERVER_URL = "http://45.77.110.58:6543";
 
 const BASE_API_URL = process.env.REACT_APP_BASE_URL;
 
